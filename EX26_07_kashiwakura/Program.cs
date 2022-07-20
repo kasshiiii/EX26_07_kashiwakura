f8fafc29f79b31e59c50ab5f0254faa414ff2c94
kasshiiii <109583121+kasshiiii@users.noreply.github.com>
2022-07-20 10:27:43 +0900

    2022/07/20

--- EX26_07_kashiwakura/Program.cs
+++ EX26_07_kashiwakura/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using InputUtilitys;
+
+public class Bos_Class
+{
+    static void Main()
+    {
+        Box box = new Box(InputUtility.InputFloat(), InputUtility.InputFloat(), InputUtility.InputFloat());
+        //Box box = new Box(10, 10, 10);
+        Console.WriteLine(box.GetSurface());
+        Console.WriteLine(box.GetVolume());
+
+    }
+}
+class Box
+{
+    float width;
+    float height;
+    float depth;
+    public Box(float width,float height,float depth)
+    {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+    public float GetSurface()
+    {
+        return width*height*6;
+    }
+    public float GetVolume()
+    {
+        return depth*width*depth;
+    }
+}
